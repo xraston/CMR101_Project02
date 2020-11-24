aad2ea59ea5eead0aafeeedf68286f0c3ae5f876
xraston <1022147@student.sae.edu.au>
2020-11-25 01:49:35 +1100

    everything working except yellow hinge joint. next create open area

---
+++ Assets/Scripts/PlasmaFloor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlasmaFloor : MonoBehaviour
+{
+	public Vector3 checkPoint;
+	public GameObject XRRig;
+	// public PlatformFall platformFall;
+
+	void OnTriggerEnter(Collider other)
+	{
+		XRRig.transform.position = checkPoint;
+		// platformFall.ResetPlatform();
+	}
+}
--- Assets/Scripts/PlatformFall.cs
+++ Assets/Scripts/PlatformFall.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class PlatformFall : MonoBehaviour
 {
-	public float fallTime = 0.5f;
+	public float dissapearTime;
+	public int reappearTime;
 
 	void OnCollisionEnter(Collision collision)
 	{
-		Debug.Log("What have I collided with? " + collision.gameObject.name);
-		if (collision.gameObject.tag == "Player")
-		{
-			Debug.Log("Collided sucessfully");
-			Invoke("Fall", fallTime);
-			//StartCoroutine(Fall(fallTime));
-		}
+		Debug.Log("Collided sucessfully");
+		Invoke("Fall", dissapearTime);
+		Invoke("ResetPlatform", reappearTime);
 	}
 
 	// add setactive true when falling through level collider
 
-	void Fall()
+	public void Fall()
 	{
 		gameObject.SetActive(false);
+		//Destroy(gameObject);
+	}
+
+	public void ResetPlatform()
+	{
+		gameObject.SetActive(true);
 		//Destroy(gameObject);
 	}
 }
